9095dfe129bc7bada7ddce19f2e4a4d4eec5d60b
Md Sifatullah <cse.sifat@gmail.com>
2019-12-07 13:34:37 +0600

    It works!!!!

--- Quize App iOS/Controller/ViewController.swift
+++ Quize App iOS/Controller/ViewController.swift
@@ -12,32 +12,99 @@ import  Foundation
 class ViewController: UIViewController {
     
     let questionBank = QuestionBank()
-    var timer = Timer()
     var counter = 0
-    var time = 31
+    var score = 0
+    var userAnswer = false
+
     
+    @IBOutlet weak var ScoreShowLbl: UILabel!
     @IBOutlet weak var QuesShowLbl: UILabel!
     @IBOutlet weak var QuesNameLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
             // UTF-8 Hexadecimal Encoding
-        timer.invalidate()
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(GetTimer), userInfo: self, repeats: true)
+        nextQuestion()
     
     }
     
-    @objc func GetTimer(){
-        counter += 1;
-        if counter < questionBank.data.count{
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        
+        if sender.tag == 1 {
+            userAnswer = true
+        }
+        else{
+            userAnswer = false
+        }
+        
+        checkAnswer()
+        
+        counter += 1
+        
+        nextQuestion()
+        
+    }
+    
+    func updateUI() {
+        
+      //  ScoreShowLbl.text = "\(score)"
+
+        
+    }
+    
+
+    func nextQuestion() {
+        
+        if counter < questionBank.data.count {
+
             QuesShowLbl.text = questionBank.data[counter].symbol
             QuesNameLbl.text = questionBank.data[counter].name
-        }
-    
-    }
+            
+            updateUI()
 
-    @IBAction func RightBtn(_ sender: UIButton) {
+        
+        } else {
+            
+
+            let alert = UIAlertController(title: "Awesome!", message: "You scored \(score)! Do you want to play again", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Play Again", style: .default, handler:
+                { (UIAlertAction) in
+                self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+
+    }
+    
+    
+    func checkAnswer() {
+        
+        let correctAnswer = questionBank.data[counter].answer
+        
+        if correctAnswer == userAnswer {
+            
+            score += 1
+            
+        }
+        
+    }
+    
+    
+    func startOver() {
+        
+        counter = 0
+        
+        score = 0
+        
+        nextQuestion()
+        
     }
     
 }
